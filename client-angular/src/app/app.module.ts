862486a7d038df0ba0e0fb47bcdaae7853ab66e7
Romarjo Balukja <mbalukja1@gmail.com>
2020-09-10 12:24:59 +0200

    added angular folder

---
+++ client-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+import { ProgressComponent } from './progress/progress.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadComponent,
+    ProgressComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
